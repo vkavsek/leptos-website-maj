076937b775ddc1fe8a8bb27290601a466339c89f
vkavsek <vkavsek@gmail.com>
2023-10-23 14:52:30 +0200

    Implemented mailing form

--- src/routes/home.rs
+++ src/routes/home.rs
@@ -1,10 +1,39 @@
 use leptos::{
-    ev::MouseEvent,
-    html::{Div, Img},
+    ev::{MouseEvent, SubmitEvent},
+    html::{Div, Input, Textarea},
     *,
 };
 use leptos_meta::{Link, Title};
-use leptos_use::{use_interval_fn, utils::Pausable};
+
+#[allow(unused)]
+const EMAIL_ADDR: &str = "kavsekmaj@gmail.com";
+
+#[allow(dead_code)]
+enum LinkLocation {
+    Fb,
+    TikTok,
+    Ig,
+}
+
+impl LinkLocation {
+    /// Returns a tuple in the form of: (HREF_TARGET, IMAGE_LOCATION)
+    fn process(&self) -> (&'static str, &'static str) {
+        match self {
+            LinkLocation::Fb => (
+                "https://www.facebook.com/majkavsek",
+                "/img/contact_icons/FB.svg",
+            ),
+            LinkLocation::TikTok => (
+                "https://www.tiktok.com/@majkavsek?lang=en",
+                "/img/contact_icons/TIKTOK.svg",
+            ),
+            LinkLocation::Ig => (
+                "https://www.instagram.com/maj.kavsek/",
+                "/img/contact_icons/insta.svg",
+            ),
+        }
+    }
+}
 
 #[component]
 pub fn Home() -> impl IntoView {
@@ -38,100 +67,6 @@ pub fn Home() -> impl IntoView {
         </footer>
     }
 }
-
-#[component]
-fn Mail() -> impl IntoView {
-    const EMAIL_ADDR: &str = "kavsekmaj@gmail.com";
-    let mailto = || format!("mailto:{}", EMAIL_ADDR);
-    let copy_mail = format!(
-        r#"function copy_mail() {{
-               // Copy the text inside the text field
-               navigator.clipboard.writeText("{}");
-            }}"#,
-        EMAIL_ADDR
-    );
-    let (show_mail, set_show_mail) = create_signal(false);
-    let (copied, set_copied) = create_signal(false);
-
-    let mail_ref = create_node_ref::<Div>();
-    let copy_ref = create_node_ref::<Img>();
-
-    let counter = create_rw_signal(0);
-    let copy_loop = move || {
-        if let Some(copy_img) = copy_ref.get() {
-            if counter.get() > 5 {
-                counter.set(0);
-                set_copied.set(false);
-                copy_img.set_src("/img/icon/copy.svg");
-            }
-            counter.update(|count| *count += 1);
-        }
-    };
-    #[allow(unused)]
-    let Pausable {
-        is_active,
-        pause,
-        resume,
-    } = use_interval_fn(copy_loop, 500);
-
-    // If copied is false stop the loop
-    create_effect(move |_| {
-        if !copied.get() {
-            pause();
-        }
-    });
-    // If copied is true start the loop
-    create_effect(move |_| {
-        if copied.get() {
-            resume();
-        }
-    });
-    let mail_click = move |ev: MouseEvent| {
-        ev.prevent_default();
-        set_show_mail.set(true);
-    };
-
-    let click_copy = move |_| {
-        let copy_img = copy_ref.get().expect("the Dom should be built");
-        copy_img.set_src("/img/icon/kluk.svg");
-        set_copied.set(true);
-    };
-
-    create_effect(move |_| {
-        leptos_use::on_click_outside(mail_ref, move |_| {
-            set_show_mail.set(false);
-        })
-    });
-
-    let close_popup = move |_| {
-        set_show_mail.set(false);
-    };
-
-    view! {
-        <script>{copy_mail}</script>
-        <a on:click=mail_click>
-            <img class="contacts-img" src="/img/contact_icons/MAIL.svg"/>
-        </a>
-        <Show when=move || show_mail.get() fallback=|| {}>
-            <div class="modal-overlay"></div>
-            <div class="modal" node_ref=mail_ref>
-                <button class="close-button" on:click=close_popup>
-                    <img src="/img/icon/cross.svg" alt="close" style:width="1rem" style:opacity="70%"/>
-                </button>
-                <a href=mailto>{EMAIL_ADDR}</a>
-                <div class="copy-and-confirm">
-                    <button class="copy-button" onclick="copy_mail()" on:click=click_copy>
-                        <img src="/img/icon/copy.svg" class="copy-img" alt="copy" node_ref=copy_ref/>
-                    </button>
-                    <Show when=move || copied.get() fallback=|| {}>
-                        <span class="span-copied">"copied"</span>
-                    </Show>
-                </div>
-            </div>
-        </Show>
-    }
-}
-
 #[component]
 fn LinkWithModal(loc: LinkLocation) -> impl IntoView {
     let (href_target, src_target) = loc.process();
@@ -178,29 +113,206 @@ fn LinkWithModal(loc: LinkLocation) -> impl IntoView {
     }
 }
 
-#[allow(dead_code)]
-enum LinkLocation {
-    Fb,
-    TikTok,
-    Ig,
+#[component]
+fn Mail() -> impl IntoView {
+    let (show_mail, set_show_mail) = create_signal(false);
+
+    let mail_ref = create_node_ref::<Div>();
+    let name_ref = create_node_ref::<Input>();
+    let email_ref = create_node_ref::<Input>();
+    let email_sub_ref = create_node_ref::<Input>();
+    let email_cont_ref = create_node_ref::<Textarea>();
+
+    const EMAIL_PATTERN: &str = r".+@.+\..+";
+
+    let mail_click = move |ev: MouseEvent| {
+        ev.prevent_default();
+        set_show_mail.set(true);
+    };
+
+    let close_popup = move |_| {
+        set_show_mail.set(false);
+    };
+
+    create_effect(move |_| {
+        leptos_use::on_click_outside(mail_ref, move |_| {
+            set_show_mail.set(false);
+        })
+    });
+
+    let mail_action =
+        create_action(|input: &(String, String, String, String)| send_mail(input.clone()));
+
+    let on_submit = move |ev: SubmitEvent| {
+        ev.prevent_default();
+        let name = name_ref
+            .get()
+            .expect("the DOM should be built by now!")
+            .value();
+        let email = email_ref.get().unwrap().value();
+        let email_sub = email_sub_ref.get().unwrap().value();
+        let email_cont = email_cont_ref.get().unwrap().value();
+
+        // TODO: content validation?
+        // No need to check if the Strings are empty, <form> does that for us.
+
+        mail_action.dispatch((name, email, email_sub, email_cont));
+    };
+
+    // TODO: Improve this
+    let (control_input, set_control_input) = create_signal(0);
+    let mail_visibility = move || {
+        let version = mail_action.version().get();
+        let mail_is_some_and_ok = mail_action
+            .value()
+            .get()
+            .unwrap_or(Err(ServerFnError::ServerError(String::new())))
+            .map(|_| true)
+            .unwrap_or_else(|_| false);
+
+        if version > control_input.get() && mail_is_some_and_ok {
+            log::info!(
+                "version changed + value is Ok\nClosing email popup, email sent sucessfully!"
+            );
+            set_show_mail.set(false);
+            set_control_input.set(version);
+        }
+        show_mail.get()
+    };
+
+    let display_mail_status = move || {
+        if mail_action.pending().get() {
+            Some(
+                view! {
+                    <div
+                        style:display="flex"
+                        style:width="100%"
+                        style="justify-content: space-evenly"
+                    >
+                        <p>"Sending..."</p>
+                        <span class="email-sending"></span>
+                    </div>
+                }
+                .into_view(),
+            )
+        } else {
+            let err = view! { <p>"Internal server error! Couldn't send the email, please try again later!"</p> }.into_view();
+            mail_action.value().get().map(|mail| match mail {
+                Ok(_) => view! { <p>"Email sent!"</p> }.into_view(),
+                Err(_) => err,
+            })
+        }
+    };
+
+    view! {
+        <a on:click=mail_click>
+            <img class="contacts-img" src="/img/contact_icons/MAIL.svg"/>
+        </a>
+        <Show when=mail_visibility fallback=|| {}>
+            <div class="modal-overlay"></div>
+            <div class="modal" node_ref=mail_ref>
+                <button class="close-button" on:click=close_popup>
+                    <img
+                        src="/img/icon/cross.svg"
+                        alt="close"
+                        style:width="1rem"
+                        style:opacity="70%"
+                    />
+                </button>
+                <form class="email-form" on:submit=on_submit>
+                    <div class="form-div">
+                        <label for="name" class="name-label">
+                            "Enter your name: "
+                        </label>
+                        <input
+                            name="name"
+                            id="name"
+                            type="text"
+                            class="name-input"
+                            node_ref=name_ref
+                            placeholder="Your Name"
+                        />
+                    </div>
+                    <div class="form-div">
+                        <label for="email" class="email-label">
+                            "Enter your email address: "
+                        </label>
+                        <input
+                            name="email"
+                            id="email"
+                            class="email-input"
+                            type="email"
+                            pattern=EMAIL_PATTERN
+                            node_ref=email_ref
+                            placeholder="email@example.com"
+                            required
+                        />
+                    </div>
+                    <div class="form-div">
+                        <label class="email-sub-label" for="email-subject">
+                            "Subject: "
+                        </label>
+                        <input
+                            class="email-sub-input"
+                            name="email-subject"
+                            id="email-subject"
+                            type="text"
+                            node_ref=email_sub_ref
+                            placeholder="Message subject"
+                            required
+                        />
+                    </div>
+                    <textarea
+                        class="email-content"
+                        name="email-content"
+                        node_ref=email_cont_ref
+                        placeholder="Your message"
+                        required
+                    ></textarea>
+                    <input class="submit-button" type="submit" value="Send"/>
+                    {display_mail_status}
+                </form>
+            </div>
+        </Show>
+    }
 }
 
-impl LinkLocation {
-    /// Returns a tuple in the form of: (HREF_TARGET, IMAGE_LOCATION)
-    fn process(&self) -> (&'static str, &'static str) {
-        match self {
-            LinkLocation::Fb => (
-                "https://www.facebook.com/majkavsek",
-                "/img/contact_icons/FB.svg",
-            ),
-            LinkLocation::TikTok => (
-                "https://www.tiktok.com/@majkavsek?lang=en",
-                "/img/contact_icons/TIKTOK.svg",
-            ),
-            LinkLocation::Ig => (
-                "https://www.instagram.com/maj.kavsek/",
-                "/img/contact_icons/insta.svg",
-            ),
-        }
+/// Takes (Name, Email, Email Subject, Email Content) as input and sends an email, returning errors
+/// if any.
+#[server(SendMail)]
+async fn send_mail(input: (String, String, String, String)) -> Result<(), ServerFnError> {
+    use lettre::{
+        message::header::ContentType, transport::smtp::authentication::Credentials,
+        AsyncSmtpTransport, AsyncTransport, Message, Tokio1Executor,
+    };
+
+    let (name, email, email_sub, email_cont) = input;
+
+    let email = Message::builder()
+        .from("Info <info@majkavsek.com>".parse().unwrap())
+        .to("Me <kavsekmaj@gmail.com>".parse().unwrap())
+        .subject(email_sub)
+        .header(ContentType::TEXT_PLAIN)
+        .body(format!(
+            "Sender INFO:\n\n{:*>20}{name}\n{:*>20}{email}\n\n\nEmail CONTENT:\n\n{email_cont}",
+            "Name: ", "Email: "
+        ))
+        .map_err(<lettre::error::Error as Into<ServerFnError>>::into)?;
+
+    let porkmail_pwd = std::env::var("PORKMAIL_PWD").unwrap_or_default();
+    let creds = Credentials::new("info@majkavsek.com".to_owned(), porkmail_pwd);
+
+    // Open a remote connection to porkbun
+    let mailer: AsyncSmtpTransport<Tokio1Executor> =
+        AsyncSmtpTransport::<Tokio1Executor>::relay("smtp.porkbun.com")?
+            .credentials(creds)
+            .build();
+
+    // Send the email
+    let res = mailer.send(email).await;
+    match res {
+        Ok(ref response) => log::info!("Sent email: {:?}", response),
+        Err(ref e) => log::error!("Error: {}", e),
     }
+    res.map(|_res| {}).map_err(|e| e.into())
 }
